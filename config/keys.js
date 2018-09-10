6b72de8e59279663265799dcf6640ed4ef47178a
Yuri Braga <32971526+yuricbraga@users.noreply.github.com>
2018-09-10 20:41:26 -0300

    Passport JWT Authentication strategy

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://yuri:bacon475@ds231589.mlab.com:31589/devconnector'
-}
+  mongoURI: "mongodb://yuri:bacon475@ds231589.mlab.com:31589/devconnector",
+  secretOrKey: "secret"
+};
